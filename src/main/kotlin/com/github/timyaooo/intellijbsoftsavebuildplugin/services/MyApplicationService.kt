602cec2b304b7b0ec5fd0b3ed6fcaef3aeffbb7d
GitHub Action <action@github.com>
2021-09-22 05:50:39 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/timyaooo/intellijbsoftsavebuildplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.timyaooo.intellijbsoftsavebuildplugin.services
+
+import com.github.timyaooo.intellijbsoftsavebuildplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
